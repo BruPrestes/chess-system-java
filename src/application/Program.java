ed9dd564a2764083c9476b73abc1e70dc5e01c14
Bruno <brunnoprestes@gmail.com>
2020-08-15 15:51:19 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+/**
+ *
+ * @author Bruno Prestes
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
